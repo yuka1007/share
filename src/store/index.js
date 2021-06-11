01538339b234376e23ac242ab7714c13a87b2f1f
yuka1007 <yuka1007@hotmail.co.jp>
2021-06-11 13:32:00 +0900

    2021.06.11.horinouchi

---
+++ src/store/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import SignUp from "../views/SignUp.vue";
+import Profile from "../views/Profile.vue";
+import Detail from "../views/Detail.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    component: Detail,
+    props: true,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex">
+    <div class="left">
+      <SideNavi />
+    </div>
+    <div class="right">
+      <div class="title">
+        <p>ホーム</p>
+      </div>
+      <Message />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNavi from "../components/SideNavi";
+import Message from "../components/Message";
+export default {
+  components: {
+    SideNavi,
+    Message
+  }
+};
+</script>
+
+<style scoped>
+.left {
+  width: 22%;
+  height: 100vh;
+}
+.right {
+  width: 78%;
+  height: 100vh;
+}
+.flex {
+  display: flex;
+}
+.title {
+  border-bottom: 1px solid white;
+  border-left: 1px solid white;
+  padding: 15px;
+}
+.title p {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
